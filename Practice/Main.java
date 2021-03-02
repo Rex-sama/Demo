7b2294ca6d6d6e244f92a4157001615e0d42fcf0
SahilGophane <sahilgophane@gmail.com>
2021-03-02 16:12:35 +0530

    demo

---
+++ Practice/Main.java
@@ -0,0 +1,10 @@
+public interface Main {
+    int amount = 10;
+    static void eatGrass(){}
+    public int  chew();
+}
+class fr3 implements Main{
+    public int chew(){
+        return 13;
+    }
+}
